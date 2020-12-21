5c8a4f64e54e9ef3c6357e7cbe15d7f142f3520c
“Omkar” <phadtareomi@gmail.com>
2020-12-21 20:13:31 +0530

    Minor changes in datastore.py

--- recommendor-system-5/datastore_directory/datastore.py
+++ recommendor-system-5/datastore_directory/datastore.py
@@ -8,13 +8,7 @@ from flask import current_app
 datastore_client = datastore.Client()
 
 def save_credentials(name, hashpass):
-    #key = datastore.Entity(key=datastore_client.key('username'),value=datastore_client.key('password'))
-    # key = datastore_client.key('credentials')
-    # q_entity = datastore.Entity(key=key)
-    # for q_prop, q_val in credentials.items():
-    #     q_entity[q_prop] = q_val
-
-    # datastore_client.put(q_entity)
+    
     kind = "users"
     #name = "username"
     users_key = datastore_client.key(kind)
@@ -23,22 +17,4 @@ def save_credentials(name, hashpass):
     users["password"] = hashpass
     datastore_client.put(users)
 
-#def get_user(name):
-    # q=users.all()
-    # q.filter("username=",name)
-    # result = q.get()
-    # return result
 
-    #query=datastore_client.query(kind="users")
-    #user_detail = list(query.fetch(username=name))
-    #return user_detail
-
-    #query=datastore_client.query(kind="users")
-    #query.add_filter("username","=",name)
-    # Res=query.fetch()
-
-    #query = datastore_client.query()
-    #res = list(query.fetch())
-
-    #query=datastore_client.query(kind="users")
-    #query.add_filter("username","=",name)
