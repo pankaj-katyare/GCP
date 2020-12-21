3e32cef4e9eefce796866c49bca65f8c8fa5efb7
Omkar <phadtareomi@gmail.com>
2020-12-21 17:25:26 +0530

    Recommendor-System-5 commited externally

---
+++ datastore_directory/datastore.py
@@ -0,0 +1,19 @@
+import os
+
+project_id = os.getenv('GCLOUD_PROJECT')
+
+from google.cloud import datastore
+from flask import current_app
+
+datastore_client = datastore.Client()
+
+def save_credentials(name, hashpass):
+   
+    kind = "users"
+    users_key = datastore_client.key(kind)
+    users = datastore.Entity(key=users_key)
+    users["username"] = name
+    users["password"] = hashpass
+    datastore_client.put(users)
+
+
